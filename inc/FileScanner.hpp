26c573bdff5b975ea7ddacab26919b1914a4ce22
kaizen857 <3064561709@qq.com>
2025-10-27 10:56:20 +0800

    FileScanner类函数声明

---
+++ inc/FileScanner.hpp
@@ -0,0 +1,52 @@
+#ifndef _FILE_SCANNER_HPP_
+#define _FILE_SCANNER_HPP_
+
+#include "MetaData.hpp"
+#include <atomic>
+#include <string>
+#include <thread>
+#include <vector>
+class FileScanner
+{
+private:
+    std::string rootDir;
+    std::vector<MetaData> items;
+
+    std::thread scanThread;
+
+    std::atomic<bool> hasScanCpld{false};
+
+    void scanDir();
+
+public:
+    FileScanner(std::string rootDir) :
+        rootDir(rootDir)
+    {
+    }
+
+    void startScan()
+    {
+        scanThread = std::thread(&FileScanner::scanDir, this);
+        scanThread.detach();
+    }
+
+    bool isScanCompleted() const
+    {
+        return hasScanCpld.load();
+    }
+
+    const std::vector<MetaData> &getItems() const
+    {
+        if (hasScanCpld.load())
+        {
+            return items;
+        }
+        else
+        {
+            static const std::vector<MetaData> empty;
+            return empty;
+        }
+    }
+};
+
+#endif
---
+++ inc/MetaData.hpp
@@ -0,0 +1,149 @@
+#ifndef _METADATA_HPP_
+#define _METADATA_HPP_
+
+#include <string>
+
+#include <taglib/tbytevector.h>
+class MetaData
+{
+private:
+    std::string title;            // 歌曲标题
+    std::string artist;           // 艺术家
+    std::string album;            // 专辑
+    std::string year;             // 出版年份
+    std::string filePath;         // 文件路径
+    std::string parentDir;        // 上级目录
+    TagLib::ByteVector coverData; // 封面数据
+
+public:
+    MetaData();
+    MetaData(const std::string &title,
+             const std::string &artist,
+             const std::string &album,
+             const std::string &year,
+             const std::string &filePath,
+             const std::string &parentDir,
+             const TagLib::ByteVector &coverData) :
+        title(title),
+        artist(artist),
+        album(album),
+        year(year),
+        filePath(filePath),
+        parentDir(parentDir),
+        coverData(coverData)
+    {
+    }
+
+    MetaData(const MetaData &other) = default;
+
+    MetaData(MetaData &&other) noexcept = default;
+
+    ~MetaData() = default;
+
+    MetaData &operator=(const MetaData &other) = default;
+
+    MetaData &operator=(MetaData &&other) noexcept = default;
+
+    std::ostream &operator<<(std::ostream &os) const
+    {
+        os << "Title: " << title << "\n"
+           << "Artist: " << artist << "\n"
+           << "Album: " << album << "\n"
+           << "Year: " << year << "\n"
+           << "File Path: " << filePath << "\n"
+           << "Parent Directory: " << parentDir << "\n"
+           << "Cover Data Size: " << coverData.size() << " bytes\n";
+        return os;
+    }
+
+    bool operator==(const MetaData &other) const
+    {
+        return title == other.title;
+    }
+    bool operator!=(const MetaData &other) const
+    {
+        return !(*this == other);
+    }
+
+    bool operator<(const MetaData &other) const
+    {
+        return title < other.title;
+    }
+
+    bool operator>(const MetaData &other) const
+    {
+        return title > other.title;
+    }
+
+    bool operator<=(const MetaData &other) const
+    {
+        return !(*this > other);
+    }
+
+    bool operator>=(const MetaData &other) const
+    {
+        return !(*this < other);
+    }
+
+    // getter
+    const std::string &getTitle() const
+    {
+        return title;
+    }
+    const std::string &getArtist() const
+    {
+        return artist;
+    }
+    const std::string &getAlbum() const
+    {
+        return album;
+    }
+    const std::string &getYear() const
+    {
+        return year;
+    }
+    const std::string &getFilePath() const
+    {
+        return filePath;
+    }
+    const std::string &getParentDir() const
+    {
+        return parentDir;
+    }
+    const TagLib::ByteVector &getCoverData() const
+    {
+        return coverData;
+    }
+
+    // setter
+    void setTitle(const std::string &title)
+    {
+        this->title = title;
+    }
+    void setArtist(const std::string &artist)
+    {
+        this->artist = artist;
+    }
+    void setAlbum(const std::string &album)
+    {
+        this->album = album;
+    }
+    void setYear(const std::string &year)
+    {
+        this->year = year;
+    }
+    void setFilePath(const std::string &filePath)
+    {
+        this->filePath = filePath;
+    }
+    void setParentDir(const std::string &parentDir)
+    {
+        this->parentDir = parentDir;
+    }
+    void setCoverData(const TagLib::ByteVector &coverData)
+    {
+        this->coverData = coverData;
+    }
+};
+
+#endif
