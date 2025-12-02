aa6c03ce47daf7992e39f80a57c655da68b2dce8
kaizen857 <3064561709@qq.com>
2025-12-02 17:48:54 +0800

    修复linux下无法使用系统文件浏览器的问题

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <QGuiApplication>
+#include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QtQuickControls2/QQuickStyle>
@@ -7,11 +8,16 @@
 #include "CoverImageProvider.hpp"
 #include <QtGlobal>
 
+#if defined(Q_OS_WIN)
 Q_DECL_EXPORT int qMain(int argc, char *argv[])
+#else
+int main(int argc, char *argv[])
+#endif
 {
     qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
 
-    QGuiApplication app(argc, argv);
+    // QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
     QQuickStyle::setStyle("Basic");
     QQmlApplicationEngine engine;
 
